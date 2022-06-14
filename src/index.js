374a40a056c9a09ff6f703346d4fa69a1daae52b
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-06-14 17:18:45 +0500

    added nw component

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import Card from './components/Card'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Card />
   </React.StrictMode>
 );
 
