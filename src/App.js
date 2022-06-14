89a7f15bc9c51c4e22c0b38cb4ac97d878a2c363
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-06-14 18:25:05 +0500

    changed

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Work <code> </code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://javohirbekkhaydarov.github.io/portfolio/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Javohirbek
         </a>
       </header>
     </div>
