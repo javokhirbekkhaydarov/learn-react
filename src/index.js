b5a775950f519db14d628ee4df813425e68d4869
javohirbekkhaydarov <javohirbekkhaydarov@gmail.com>
2022-06-14 17:23:40 +0500

    just this is simple test

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import Card from './components/Card'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card />
+    <App />
   </React.StrictMode>
 );
 
